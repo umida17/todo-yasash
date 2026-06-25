d1d2f9dbe5d49e852ec643ecf678eb04f272c32a
uy <a.normurzaev20@gmail.com>
2026-06-26 00:55:22 +0500

    todo yasaw

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+ import React, { useState } from 'react'
+ 
+ const App = () => {
+  const [todo, setTodo]= useState("");
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default App
  
